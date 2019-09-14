89d1b2281c718b2590416ca4dd50c88488ab45b6
akhil <akhil.nair16@siesgst.ac.in>
2019-09-14 12:37:35 +0530

    New Changes

--- app/src/main/java/com/miniproject/kitchenautomation/login_activity.java
+++ app/src/main/java/com/miniproject/kitchenautomation/login_activity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
